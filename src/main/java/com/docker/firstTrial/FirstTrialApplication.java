f0551d4d55788fdb5371e2429647c96509c08033
Abinit Varma <abinitvarma1024@gmail.com>
2020-05-24 01:45:32 +0530

    First Commit

---
+++ src/main/java/com/docker/firstTrial/FirstTrialApplication.java
@@ -0,0 +1,13 @@
+package com.docker.firstTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTrialApplication.class, args);
+	}
+
+}
